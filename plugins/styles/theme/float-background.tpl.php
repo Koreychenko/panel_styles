4a99077e04a9b4356859011379e1958e9ea6c9ec
Max Koreychenko <koreychenko@gmail.com>
2015-09-15 14:34:32 +0300

    add float style and advanced style parameters

--- plugins/styles/theme/float-background.tpl.php
+++ plugins/styles/theme/float-background.tpl.php
@@ -2,16 +2,18 @@
 <?php if ($pane_prefix): ?>
   <?php print $pane_prefix; ?>
 <?php endif; ?>
+<div class="float-content-<?php print $position; ?>">
 <?php if ($title): ?>
   <h2 class="pane-title"><?php print $title; ?></h2>
 <?php endif; ?>
 <?php if ($admin_links): ?>
     <?php print $admin_links; ?>
   <?php endif; ?>
-  <div class="container">
+
     <?php print render($content); ?>
-  </div>
+
   <?php if ($pane_suffix): ?>
   <?php print $pane_suffix; ?>
 <?php endif; ?>
+</div>
 </div>
